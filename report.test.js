6d6be220059c23432a80734c97efc693d1bf725b
alexanu4 <alexdmarchis@gmail.com>
2026-01-07 14:44:36 +0200

    printReport finished

---
+++ report.test.js
@@ -0,0 +1,33 @@
+const { sortPages } = require('./report.js');
+const { test, expect } = require('@jest/globals')
+
+test('sortPages 2 pages', () => {
+    const input = {
+        'https://blog.boot.dev/path': 1,
+        'https://blog.boot.dev': 3
+    };
+    const actual = sortPages(input);
+    const expected = [['https://blog.boot.dev', 3],
+    ['https://blog.boot.dev/path', 1]];
+
+    expect(actual).toEqual(expected);
+});
+
+test('sortPages 5 pages', () => {
+    const input = {
+        'https://blog.boot.dev/path': 1,
+        'https://blog.boot.dev': 3,
+        'https://blog.boot.dev/path1': 5,
+        'https://blog.boot.dev/path2': 2,
+        'https://blog.boot.dev/path3': 9
+    };
+    const actual = sortPages(input);
+    const expected = [['https://blog.boot.dev/path3', 9],
+    ['https://blog.boot.dev/path1', 5],
+    ['https://blog.boot.dev', 3,],
+    ['https://blog.boot.dev/path2', 2],
+    ['https://blog.boot.dev/path', 1]
+    ];
+
+    expect(actual).toEqual(expected);
+});
